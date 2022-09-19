83cb873ee7c1b1b92484c4671aa997e2ed6e5dbd
hikikun11 <hamwd21@gmail.com>
2022-09-19 14:24:09 +0300

    uploading files

---
+++ HomeScript.js
@@ -0,0 +1,57 @@
+let Recroding = false
+let PermissionGived = false
+function RecordAudio(stream) 
+{
+    if (window.URL) {
+        RecordPlayer.srcObject = stream;
+    } else {
+        RecordPlayer.src = stream;
+    }
+}
+
+function RecordBTClicked(e)
+{
+    var FirstP = document.getElementById("FirstPID")
+    if(!Recroding)
+    {
+        RecordMic()
+        if(PermissionGived)
+        {
+            FirstP.innerHTML = "Recording... (Click Record button Again to stop recording)"
+            Recroding = true
+        }
+    }
+    else
+    {
+        FirstP.innerHTML = "Stoped Recording"
+        Recroding = false
+        StopRecording()
+    } 
+}
+
+function RecordMic()
+{
+    var RecordPlayer = document.getElementById("RecordPlayer"); 
+    var FirstP = document.getElementById("FirstPID")
+        navigator.mediaDevices
+        .getUserMedia({audio: true, video: false})
+        .then(RecordAudio)
+        .catch(function (error)
+        {
+            PermissionGived = false
+            FirstP.innerHTML = "Please Give Permission!"
+            Recroding = false
+        });
+        PermissionGived = true
+        if(!Recroding)
+        {
+            StopRecording()
+            return
+        }
+          
+}
+
+function StopRecording()
+{
+    RecordPlayer.srcObject = null;
+}
